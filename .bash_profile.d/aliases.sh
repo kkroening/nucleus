16839964a82b82a68b5b4bf949d2b082201e3fe4
Karl Kroening <karlk@kralnet.us>
2021-07-26 14:30:04 -0700

    Update bash aliases

--- .bash_profile.d/aliases.sh
+++ .bash_profile.d/aliases.sh
@@ -3,18 +3,17 @@
 nucleus::_setup_aliases() {
   unset -f nucleus::_setup_aliases
 
-  alias vc='virtualenv venv'
-  alias va='. venv/bin/activate'
-  alias va3='. venv3/bin/activate'
-  alias vd='deactivate'
-  alias ls='ls --color=auto'
-  alias less='less -FRX'
   alias ..='cd ..'
-
+  alias less='less -FRX'
+  alias lla='ls -la'
+  alias ls='ls --color=auto'
   alias pbc='pbcopy'
   alias pbp='pbpaste'
-
   alias tf=terraform
+  alias va3='. venv3/bin/activate'
+  alias va='. venv/bin/activate'
+  alias vc='virtualenv venv'
+  alias vd='deactivate'
 }
 
 nucleus::_setup_aliases
