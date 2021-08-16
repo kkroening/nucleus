b7865e4ccb705e9a24743f230df3dd492edb8d0f
Karl Kroening <karlk@kralnet.us>
2021-08-16 16:13:23 -0700

    Fix pyenv

--- .bash_profile.d/python.sh
+++ .bash_profile.d/python.sh
@@ -3,10 +3,15 @@
 nucleus::_setup_python() {
   unset -f nucleus::_setup_python
 
-  if [[ -d ~/.pyenv/bin ]]; then
-    PATH="${PATH}:~/.pyenv/bin"
+  export PYENV_ROOT="${HOME}/.pyenv"
+  if [[ -d "${PYENV_ROOT}/bin" ]]; then
+    # Note: Not all pyenv installations have a `bin` directory in
+    # `${PYENV_ROOT}`; e.g. pyenv installed via homebrew has the pyenv binary
+    # in `/usr/local/bin`, in which case this section is skipped.
+    PATH="${PATH}:${PYENV_ROOT}/bin"
   fi
   if which pyenv > /dev/null; then
+    eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
     export CLOUDSDK_PYTHON="$(pyenv shell 2.7.14 && pyenv which python)"
   fi
