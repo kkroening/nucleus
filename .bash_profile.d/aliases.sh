f245bff266e5d753b5d113ef5fae1173ff3d1d6a
Karl Kroening <karlk@kralnet.us>
2026-02-08 23:28:22 -0600

    Make pbp+pbc work on Windows WSL

--- .bash_profile.d/aliases.sh
+++ .bash_profile.d/aliases.sh
@@ -10,8 +10,6 @@ nucleus::_setup_aliases() {
   alias less='less -FRX'
   alias lla='ls -la'
   alias ls='ls --color=auto'
-  # alias pbc='pbcopy'
-  alias pbp='pbpaste'
   alias tf=terraform
   alias va3='. venv3/bin/activate'
   alias .va='. .venv/bin/activate'
